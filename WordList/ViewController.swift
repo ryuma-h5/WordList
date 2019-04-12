f22528de6a0f3502814676c822b4009519c372aa
Ryuma.h <ryuma810dragon@gmail.com>
2019-04-12 21:53:46 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Ryuma Harada on 2019/04/12.
+//  Copyright © 2019 Ryuma Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
